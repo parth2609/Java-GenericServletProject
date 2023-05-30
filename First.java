4730ec5132bf6072e373fe3b43d039a9f50185e3
Parth Garg <84276178+parth2609@users.noreply.github.com>
2023-05-31 05:01:25 +0530

    Add files via upload
    Signed-off-by: Parth Garg <84276178+parth2609@users.noreply.github.com>

---
+++ First.java
@@ -0,0 +1,16 @@
+import java.io.*;  
+import javax.servlet.*;  
+  
+public class First extends GenericServlet{  
+public void service(ServletRequest req,ServletResponse res)  
+throws IOException,ServletException{  
+  
+res.setContentType("text/html");  
+  
+PrintWriter out=res.getWriter();  
+out.print("<html><body>");  
+out.print("<b>hello generic servlet</b>");  
+out.print("</body></html>");  
+  
+}  
+}  
